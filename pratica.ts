6233b184dc6559025eca5aa4235b058ac0825465
Luciano Brito <lucianocassio66@gmail.com>
2024-06-27 16:18:24 -0300

    first commit

---
+++ pratica.ts
@@ -0,0 +1,10 @@
+function multiplica(a: number, b: number): number {
+    return a * b;
+}
+
+function sauda(name: string): string {
+    return `Olá ${name}`;
+}
+
+const resultado = multiplica(3, 4);
+const saudacao = sauda("João");
